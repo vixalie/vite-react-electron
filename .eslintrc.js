c229dd477baf090e2146369edbd38259f047ffbc
徐涛 <midnite@outlook.com>
2022-05-06 10:16:36 +0800

    build(lint):增加ESLint依赖和配置。

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json', './tsconfig.main.json', './tsconfig.node.json']
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking'
+  ]
+};
